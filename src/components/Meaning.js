81fd9a013d9904ed544f5dccd5d8850a18a377b9
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-10-05 17:23:25 +0300

    Removed component Synonyms
    incorporated synonyms into Meaning component instead

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -1,20 +1,26 @@
 import React from "react";
-import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning(props) {
+  console.log(props);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-
+      <h3 className="mb-2">{props.meaning.partOfSpeech}</h3>
+      <div className="Synonym-box">
+        {props.meaning.synonyms.map(function (synonym, index) {
+          return (
+            <ul className="Synonyms">
+              <li key={index}> {synonym}</li>
+            </ul>
+          );
+        })}
+      </div>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <div className="definition mt-4"> {definition.definition}</div>
 
             <div className="example">{definition.example}</div>
-
-            <Synonyms synonyms={props.meaning.synonyms} />
           </div>
         );
       })}
