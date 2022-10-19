0a60b1ef374cd8d6ae5341ddcc2a68417d11c61c
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-10-19 22:33:13 +0300

    trying to play audio on the same page

--- src/components/Phonetic.js
+++ src/components/Phonetic.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  console.log(props);
+  const audioRef = useRef(props.phonetics[0].audio);
+
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    (audioRef) => setClicked(false);
+    if (clicked) {
+      audioRef();
+    } else {
+      return null;
+    }
+  });
+
   return (
     <div className="Phonetic mt-4">
-      <span>
-        {" "}
-        <a href={props.phonetics[0].audio} target="_blank" rel="noreferrer">
-          Listen
-        </a>{" "}
-      </span>
+      <button onClick={() => setClicked}>Listen</button>
 
       <span className="phonetics ms-4">{props.phonetics[0].text} </span>
     </div>
